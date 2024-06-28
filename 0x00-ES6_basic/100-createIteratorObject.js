600fe77025d5072657e310b747881c274f5075eb
Peace-lov <chidieberepeace42@gmail.com>
2024-06-28 09:21:00 +0000

    createiterator

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   const allEmployees = report.allEmployees;
   const employees = [];
 
-  for (const department of object.keys(allEmployees)) {
+  for (const department of Object.keys(allEmployees)) {
     employees.push(...allEmployees[department]);
   }
 
