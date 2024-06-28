0d346aa293de8e4450b9886de87f7481c1a448f7
Peace-lov <chidieberepeace42@gmail.com>
2024-06-28 05:57:23 +0000

    budget

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget[`income-${getCurrentYear(new Date())}`] = income;
-  budget[`gdp-${getCurrentYear(new Date())}`] = gdp;
-  budget[`capita-${getCurrentYear(new Date())}`] = capita;
-
+  const CurrYear = getCurrentYear();
+  const budget = {
+	  [`income-${CurrYear}`]:income,
+	  [`gdp-${CurrYear}`]:gdp,
+	  [`capita-${CurrYear}`]:capita,
+  };
   return budget;
 }
