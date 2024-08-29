33d57e5b58d0693b070b119bda1d524ab4d74e9d
Peace-lov <chidieberepeace42@gmail.com>
2024-08-29 11:46:31 +0000

    9-try

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,7 +6,7 @@ guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push(Stringerr));
+    queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');
   }
