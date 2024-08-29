fbd432082ab2bee4df33016164313d8131bde9f9
Peace-lov <chidieberepeace42@gmail.com>
2024-08-29 11:10:45 +0000

    message

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function
 handleProfileSignup(firstName, lastName, fileName) {
@@ -10,5 +10,5 @@ handleProfileSignup(firstName, lastName, fileName) {
         status: o.status,
         value: o.status === 'fulfilled' ? o.value : String(o.reason),
       }))
-  ));
+    ));
 }
