f8650cc2c1877bb57950cc341074426fd271beb4
Peace-lov <chidieberepeace42@gmail.com>
2024-10-02 09:41:33 +0000

    update building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft = 0) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage != 'function') {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
