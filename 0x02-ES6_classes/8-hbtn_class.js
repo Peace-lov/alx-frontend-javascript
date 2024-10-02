a05e1ae11a16f96b65e90e407896af7b789f4c67
Peace-lov <chidieberepeace42@gmail.com>
2024-10-02 10:04:05 +0000

    update hbtn class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,7 +1,7 @@
 export default class HolbertonClass {
-  constructor(size, loaction) {
+  constructor(size, location) {
     if (typeof size !== 'number') {
-      throw new TypeError('Size must be a number')
+      throw new TypeError('Size must be a number');
     } else if (typeof location !== 'string') {
       throw new TypeError('Location must be a string');
     }
